5adc0f09e484fe2258b957df1827d53d1ddfb270
Wilyos <wilyos99@gmail.com>
2025-04-02 13:20:03 -0500

    reincorporacion

--- obj/Debug/net9.0/NewDawn.AssemblyInfo.cs
+++ obj/Debug/net9.0/NewDawn.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewDawn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d25134b20ab3ca72a84c58d915a3d25da87828b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1a527a77bb9e93f08cc92dc4d0f8683fd6e27ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewDawn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewDawn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
