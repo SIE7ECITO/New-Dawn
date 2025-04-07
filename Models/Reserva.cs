4c61418cb9e1075bbe3ff7a28295e9649a8f2a87
SIE7ECITO <SIERRAOSORIOJUANDIEGO@GMAIL.COM>
2025-04-07 16:06:35 -0500

    Reservas y pagos

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -13,13 +13,13 @@ public partial class Reserva
 
     public int? Idpaquete { get; set; }
 
-    public int? Idpago { get; set; }
+    public int Idpago { get; set; }
 
     public DateOnly FechaReserva { get; set; }
 
-    public DateOnly FechaComienzo { get; set; }
+    public DateOnly? FechaComienzo { get; set; }
 
-    public DateOnly FechaFin { get; set; }
+    public DateOnly? FechaFin { get; set; }
 
     public double ValorTotal { get; set; }
 
@@ -27,15 +27,15 @@ public partial class Reserva
 
     public virtual ICollection<HabitacionReserva> HabitacionReservas { get; set; } = new List<HabitacionReserva>();
 
-    public virtual ICollection<HuespedReserva> HuespedReservas { get; set; } = new List<HuespedReserva>();
+    public virtual ICollection<HuespedReserva>? HuespedReservas { get; set; } = new List<HuespedReserva>();
 
-    public virtual Habitacion IdhabitacionNavigation { get; set; } = null!;
+    public virtual Habitacion? IdhabitacionNavigation { get; set; } = null!;
 
     public virtual Pago? IdpagoNavigation { get; set; }
 
     public virtual Paquete? IdpaqueteNavigation { get; set; }
 
-    public virtual Usuario IdusuarioNavigation { get; set; } = null!;
+    public virtual Usuario? IdusuarioNavigation { get; set; } = null!;
 
     public virtual ICollection<Servicio> Idservicios { get; set; } = new List<Servicio>();
     public virtual ICollection<ReservaServicio> ReservaServicios { get; set; } = new List<ReservaServicio>();
