df0f5264a7e6a17a7537f1b5001c33252d92aac1
SIE7ECITO <SIERRAOSORIOJUANDIEGO@GMAIL.COM>
2025-04-08 15:01:02 -0500

    Busqueda H

--- obj/Debug/net9.0/NewDawn.AssemblyInfo.cs
+++ obj/Debug/net9.0/NewDawn.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewDawn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b208dd56f17bd423325de129bc7beee170d7af15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d9f4b24ab9d50b18ed3a12e8d1169526edec8f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewDawn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewDawn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
