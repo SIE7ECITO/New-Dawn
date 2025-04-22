de367ce30697e29d4350b4883d08218f8efde3e4
SIE7ECITO <SIERRAOSORIOJUANDIEGO@GMAIL.COM>
2025-04-22 01:50:33 -0500

    si

--- obj/Debug/net9.0/NewDawn.AssemblyInfo.cs
+++ obj/Debug/net9.0/NewDawn.AssemblyInfo.cs
@@ -16,10 +16,14 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 <<<<<<< HEAD
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+038874b6e226d85a4b4f20943845df4018e77a06")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea703bea2f756c10c59dafedd6d70389bf3c2805")]
 >>>>>>> Reservas de nuevo
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca206b528e12a00765bc90cfb5bc0566f86bd0d7")]
+>>>>>>> si
 [assembly: System.Reflection.AssemblyProductAttribute("NewDawn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewDawn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
